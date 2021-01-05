456b2c70c7fafe3158591d79d2cffd5103e68b2e
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-01-04 22:58:25 -0300

    Criado estrutura microserviço desafios (challenge)

---
+++ service-challenge/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { RpcExceptionFilter } from './filters/rpc-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@192.168.99.100:5672/smart-ranking'],
+      queue: 'challenge',
+      queueOptions: {
+        durable: true,
+      },
+      noAck: false,
+    },
+  })
+
+  app.useGlobalFilters(new RpcExceptionFilter())
+
+  await app.listenAsync()
+}
+bootstrap();
